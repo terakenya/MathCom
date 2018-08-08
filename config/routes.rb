0e5f630539bd5eefd1b579fab657cdc43d203d41
terakenya <acmacm10@icloud.com>
2018-08-08 15:40:54 +0900

    Create resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
-  get 'users' => 'users#index'
-  get 'articles' => 'articles#index'
-  get 'articles/new' => 'articles#new'
   devise_scope :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-  get 'articles/search' => 'articles#search'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get 'articles/search' => 'articles#search'
   end
+  resources :articles
+  resources :users
 end
