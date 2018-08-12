f4f01d4f7e24fac5693dd3bbd058735465b58a1f
terakenya <acmacm10@icloud.com>
2018-08-12 15:50:40 +0900

    Change_article⇨question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController
+class QuestionsController < ApplicationController
   def index
     if (!params[:keywordSearch])
       if (!params[:generation])
@@ -13,14 +13,26 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Article.new(question_params)
+    @question.save
   end
 
   def show
   end
+
   def search
     @questions = Question.where('title LIKE(?) or description LIKE(?)', "%#{params[:keyword]}%", "%#{params[:keyword]}%").limit(1)
     respond_to do |format|
       format.json
     end
+  end
+
+  private
+  def question_params
+    params.require(:article).permit(:image, :text, :title).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'articles#index'
+  root 'questions#index'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
     get 'articles/search' => 'articles#search'
   end
   resources :articles
+  resources :questions
   resources :users
 end
