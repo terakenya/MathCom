739f63eed0c43b53e9d94a3d62694b23e5d3b4e4
terakenya <acmacm10@icloud.com>
2018-08-27 17:33:08 +0900

    タグ機能やり直し

--- db/migrate/20180827071306_create_question_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateQuestionCategories < ActiveRecord::Migration
-  def change
-    create_table :question_categories do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180827045649) do
+ActiveRecord::Schema.define(version: 20180827071306) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
+
+  create_table "question_categories", force: :cascade do |t|
+    t.integer  "question_id", limit: 4
+    t.integer  "category_id", limit: 4
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  add_index "question_categories", ["category_id"], name: "index_question_categories_on_category_id", using: :btree
+  add_index "question_categories", ["question_id", "category_id"], name: "index_question_categories_on_question_id_and_category_id", unique: true, using: :btree
+  add_index "question_categories", ["question_id"], name: "index_question_categories_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -24,7 +43,7 @@ ActiveRecord::Schema.define(version: 20180827045649) do
     t.datetime "updated_at"
   end
 
-  add_index "questions", ["user_id"], name: "fk_rails_047ab75908", using: :btree
+  add_index "questions", ["user_id", "created_at"], name: "index_questions_on_user_id_and_created_at", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
