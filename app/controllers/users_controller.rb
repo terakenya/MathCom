de0d4751f133fda5d6d10f02699d7a758608abd1
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-09-20 11:51:23 +0900

    controller 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,3 +5,70 @@ class UsersController < ApplicationController
     @user = User.find(current_user)
   end
 end
+
+
+
+class UsersController < ApplicationController
+
+  layout "main"
+
+  before_action :move_to_session, except: :index
+
+  def show
+    @user = User.find(params[:id])
+    @id = @user.id
+    @articles = Article.all
+  end
+
+  def like
+    @user = User.find(params[:user_id])
+    @id = @user.id
+    @articles = Article.all
+  end
+
+  def private
+    @user = User.find(params[:user_id])
+    @id = @user.id
+    @articles = Article.all
+  end
+
+  def commentpage
+    @user = User.find(params[:user_id])
+    @id = @user.id
+    @articles = Article.all
+  end
+
+  def update
+    @user = current_user
+    judge = params[:judge]
+
+    if judge == "0"
+      @user.update(user_params)
+      @tags = params[:tag]
+      @tag = params[:this_tag]
+    else
+      @user.update(user_image_params)
+      redirect_to action: :show
+    end
+  end
+
+  private
+  def user_params
+    params.permit(
+      :name,
+      :email
+    ).merge(tag_list: params[:tag])
+  end
+
+  def user_image_params
+    params.permit(
+      :name,
+      :email
+    ).merge(image: params[:user][:image])
+  end
+
+  def move_to_session
+    redirect_to "/users/sign_in" unless user_signed_in?
+  end
+
+end
